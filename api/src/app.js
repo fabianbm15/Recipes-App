c874af944ffaae1579c8bb46b3c0a1609532a6ac
fabianbm15 <cmelgarejobm@gmail.com>
2023-04-01 16:32:18 -0500

    fixed cors

--- api/src/app.js
+++ api/src/app.js
@@ -15,7 +15,7 @@ app.use(bodyParser.json({ limit: "50mb" }));
 app.use(cookieParser());
 app.use(morgan("dev"));
 app.use((req, res, next) => {
-   res.header("Access-Control-Allow-Origin", "https://foodcook-fabian.onrender.com/"); // update to match the domain you will make the request from
+   res.header("Access-Control-Allow-Origin", "https://foodcook-fabian.netlify.app"); // update to match the domain you will make the request from
    res.header("Access-Control-Allow-Credentials", "true");
    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
    res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
